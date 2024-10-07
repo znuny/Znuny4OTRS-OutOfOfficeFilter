edaa840d92041653b74f447522b8b5e1f8c53565
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2024-10-07 09:02:40 +0000

    Added additional ObjectLog (debug) for each matching filter and when all filters have been checked.

--- Kernel/System/PostMaster/Filter/PreZnunyOutOfOfficeFilter.pm
+++ Kernel/System/PostMaster/Filter/PreZnunyOutOfOfficeFilter.pm
@@ -28,6 +28,9 @@ sub new {
 
     $Self->{Debug} = $Param{Debug} || 0;
 
+    # Get communication log object.
+    $Self->{CommunicationLogObject} = $Param{CommunicationLogObject} || die "Got no CommunicationLogObject!";
+
     # Default Settings
     $Self->{Config} = {};
 
@@ -56,6 +59,8 @@ sub Run {
     FILTER:
     for my $Filter ( @{$FilterAttributes} ) {
 
+        my $MatchedAttribute;
+        my $MatchedRegEx;
         for my $Attribute ( sort keys %{$Filter} ) {
 
             my $AttributeLC = lc($Attribute);
@@ -70,9 +75,29 @@ sub Run {
                 # check header value via regex
                 next FILTER if $TempGetParam{$AttributeLC} !~ m{$RegEx}i;
             }
+
+            $MatchedAttribute = $AttributeLC;
+            $MatchedRegEx     = $RegEx;
         }
 
         $Param{GetParam}->{$KeepStateHeader} = 1;
+
+        $Self->{CommunicationLogObject}->ObjectLog(
+            ObjectLogType => 'Message',
+            Priority      => 'Debug',
+            Key           => 'Kernel::System::PostMaster::Filter::PreZnunyOutOfOfficeFilter',
+            Value => "KeepStateHeader $KeepStateHeader was set. Attribute: $MatchedAttribute | RegEx: $MatchedRegEx",
+        );
+    }
+
+    # All filters have been checked.
+    if ( @{$FilterAttributes} ) {
+        $Self->{CommunicationLogObject}->ObjectLog(
+            ObjectLogType => 'Message',
+            Priority      => 'Debug',
+            Key           => 'Kernel::System::PostMaster::Filter::PreZnunyOutOfOfficeFilter',
+            Value         => "All filters have been checked.",
+        );
     }
 
     return 1;
